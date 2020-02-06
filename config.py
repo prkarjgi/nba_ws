a8891d706bd09caefcd9b9ad2d3a3ca52b55992b
prkarjgi <pkarajgikar@gmail.com>
2020-02-06 14:45:45 +0530

    Removed smtp variables from config

--- config.py
+++ config.py
@@ -9,14 +9,8 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
         'this-is-a-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(base_dir, 'flask-app.db')
+        'sqlite:///' + os.path.join(base_dir, 'nba-news.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['prkarjgi@gmail.com']
 
 
 class ProductionConfig(Config):
